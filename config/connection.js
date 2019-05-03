75179286f5cb5b592a1cf4eac7a396257fb608d0
tony <tmonnie@stp-er-10-129-70-26.wireless.umn.edu>
2019-05-02 20:06:28 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var connection; 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
-
-  
+}
+connection.connect();
   module.exports = connection;
